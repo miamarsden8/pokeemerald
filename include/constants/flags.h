9e700051c9a9431a162ec09ad497038b52467a62
Mia Marsden <miamarsden8@gmail.com>
2025-04-19 17:18:36 -0400

    Move relearner and starting item moved

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -528,9 +528,9 @@
 #define FLAG_HIDE_FIVE_ISLAND_LOST_CAVE_ROOM14_RARE_CANDY       0x1FC
 #define FLAG_HIDE_SEVEN_ISLAND_SEVAULT_CANYON_HOUSE_LUCKY_PUNCH 0x1FD
 #define FLAG_HIDE_SILPH_CO_4F_TM41                              0x1FE
+#define FLAG_HIDE_LAB_ITEM                                      0x1FF
 
 // Unused?
-#define FLAG_0x1FF               0x1FF
 #define FLAG_0x200               0x200
 #define FLAG_0x201               0x201
 #define FLAG_0x202               0x202
